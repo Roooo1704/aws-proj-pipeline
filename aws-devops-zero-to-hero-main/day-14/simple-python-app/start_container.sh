9b2c38829c655f03787c21dcfb526e3e20c1f255
Roooo1704 <151621563+Roooo1704@users.noreply.github.com>
2024-07-22 17:39:38 +0530

    Update start_container.sh

--- aws-devops-zero-to-hero-main/day-14/simple-python-app/start_container.sh
+++ aws-devops-zero-to-hero-main/day-14/simple-python-app/start_container.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-set -e
+#set -e
 
 # Pull the Docker image from Docker Hub
-docker pull rohit256/test:tagname
+#docker pull rohit256/test:tagname
 
 # Run the Docker image as a container
-docker run -d -p 80:80 rohit256/test:tagname
+#docker run -d -p 80:80 rohit256/test:tagname
+pip install -r day-13/simple-python-app/requirements.txt
