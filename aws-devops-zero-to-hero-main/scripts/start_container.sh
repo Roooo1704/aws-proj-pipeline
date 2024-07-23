f13fa21fbb7fcaeea88bc6c88e80bf79c988fec4
Roooo1704 <151621563+Roooo1704@users.noreply.github.com>
2024-07-23 10:03:42 +0530

    Delete aws-devops-zero-to-hero-main/scripts/start_container.sh

--- aws-devops-zero-to-hero-main/scripts/start_container.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-# Pull the Docker image from Docker Hub
-docker pull rohit256/test:tagname
-
-# Run the Docker image as a container
-docker run -d -p 5000:5000 rohit256/test:tagname
-
